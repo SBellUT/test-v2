470b310c9836b23a1c7270d765489bc5c0221173
SBellUT <111801926+SBellUT@users.noreply.github.com>
2022-09-14 13:02:10 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 input.onPinPressed(TouchPin.P0, function () {
     max7219_matrix.clearAll()
+    max7219_matrix.brightnessAll(0)
 })
 input.onPinReleased(TouchPin.P0, function () {
-    max7219_matrix.displayTextAlignRight(
-    happySad,
-    true
-    )
+    control.reset()
 })
 input.onButtonPressed(Button.A, function () {
     if (!(input.pinIsPressed(TouchPin.P0))) {
@@ -18,7 +16,19 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onPinPressed(TouchPin.P1, function () {
     if (!(input.pinIsPressed(TouchPin.P0))) {
-        playExplode = playExplode * -1
+        if (happySad == ":)") {
+            music.playTone(349, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(220, music.beat(BeatFraction.Double))
+            happySad = ":("
+        } else {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(523, music.beat(BeatFraction.Double))
+            happySad = ":)"
+        }
         max7219_matrix.displayTextAlignRight(
         happySad,
         true
@@ -30,12 +40,11 @@ let degrees = 0
 let voltage = 0
 let sensorVal = 0
 let happySad = ""
-let playExplode = 0
 let randomAnimation = 0
 let vcc = 3.3
 let full_angle = 300
 randomAnimation = 1
-playExplode = 1
+let playExplode = 1
 let makeNoise = 1
 happySad = ":)"
 max7219_matrix.setup(
@@ -62,6 +71,7 @@ basic.forever(function () {
     degrees = Math.idiv(voltage * full_angle, vcc)
     brightness = Math.map(degrees, 0, full_angle, 0, 255)
     max7219_matrix.brightnessAll(brightness)
+    music.setVolume(brightness)
     basic.pause(500)
 })
 basic.forever(function () {
